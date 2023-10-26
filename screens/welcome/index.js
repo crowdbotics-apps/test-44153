080ad334cf907dc0b4f529d6374a228f06ddad6e
R  Vickrama <r.vickrama.dms@crowdbotics.com>
2023-10-26 18:48:29 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/welcome/index.js
+++ screens/welcome/index.js
@@ -1,17 +1,15 @@
+import { TextInput } from "react-native";
 import React from "react";
-import { View, Image, Text, ScrollView, SafeAreaView, StyleSheet } from "react-native";
+import { View, Text, ScrollView, SafeAreaView, StyleSheet } from "react-native";
 
 const WelcomeScreen = () => {
   return <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollView}>
         <View style={styles.group} />
-        <View style={styles.group}>
-          <Image style={styles.logo} source={require("./logo.png")} />
-          <Text style={styles.text}>
-            Let's build something amazing together!
-          </Text>
-        </View>
+
         <Text style={styles.footer}>Made with ❤️ by Crowdbotics</Text>
+        <TextInput style={styles.tsyyofgG} value="user"></TextInput>
+        <Text style={styles.ZKZDxvyR}>Lorem ipsum…</Text>
       </ScrollView>
     </SafeAreaView>;
 };
@@ -30,25 +28,25 @@ const styles = StyleSheet.create({
   group: {
     alignItems: "center"
   },
-  logo: {
-    height: 180,
-    width: 180,
-    padding: 40,
-    borderRadius: 30,
-    margin: 40
-  },
-  text: {
-    textAlign: "center",
-    fontSize: 28,
-    color: "#828AB0",
-    fontWeight: 700
-  },
   footer: {
     textAlign: "center",
     fontSize: 18,
     color: "#828AB0",
     fontWeight: 700,
     marginBottom: 20
+  },
+  tsyyofgG: {
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 30
+  },
+  ZKZDxvyR: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
   }
 });
 export default WelcomeScreen;
